0ac822b8a7c0b64127963750ff66b956c1e31b6a
Stéphane Francel <contact@avine.io>
2017-04-24 16:21:32 +0200

    Update cv...

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,8 +4,8 @@
 export default function ($) {
 
   var tel = ["06", "18", "30", "86", "11"],
-    mail = ["stephane" + "." + "francel" + "@" + "gmail" + "." + "com"].join("");
-  $("#fill-tel").attr("href", "tel:" + tel.join("")).text(tel.join(" "));
-  $("#fill-mail").attr("href", "mailto:" + mail).text(mail);
+    mail = ["step" + "hane" + "." + "fra" + "ncel" + "@" + "gm" + "ail" + "." + "c" + "om"].join("");
+  $("#fill-tel").attr("href", "te" + "l:" + tel.join("")).text(tel.join(" "));
+  $("#fill-mail").attr("href", "ma" + "ilto:" + mail).text(mail);
 
 }
